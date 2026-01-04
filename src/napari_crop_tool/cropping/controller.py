456aae8caf7bebbbefab54535e085f9e86faf9c5
girochat <117285316+girochat@users.noreply.github.com>
2026-01-04 19:20:41 +0100

    Feature: cropping also in XZ and YZ planes

--- src/napari_crop_tool/cropping/controller.py
+++ src/napari_crop_tool/cropping/controller.py
@@ -24,6 +24,7 @@ class CroppingController:
 
         # Wire napari + gui events
         self.model.shapes_layer.events.data.connect(self.update_rois)
+        self.model.viewer.dims.events.point.connect(self._project_shapes)
         self.gui.set_start_btn.clicked.connect(self.on_set_start)
         self.gui.set_stop_btn.clicked.connect(self.on_set_stop)
         self.gui.clear_rois_btn.clicked.connect(self.on_clear_rois)
@@ -32,25 +33,48 @@ class CroppingController:
         # Initial paint
         self.update_rois()
 
+    def _project_shapes(self):
+        curr_shapes_data = self.model.shapes_layer.data
+        n = self.model.num_rois()
+        curr_axis = self.model.viewer.dims.order[0]
+        for i in range(n):
+            scroll_roi_axis = self.model.get_track_axis(i)
+            if curr_axis == scroll_roi_axis:
+                slice_idx = self.model.viewer.dims.current_step[curr_axis]
+                curr_shapes_data[i][:,curr_axis] = slice_idx        
+        self.model.shapes_layer.data = curr_shapes_data
+
     def update_rois(self, *args):
         n = self.model.num_rois()
+
+        if len(self.gui.rois_gui) < n:
+            n_new_rois = n - len(self.gui.rois_gui)
+            scroll_axis = self.model.viewer.dims.order[0]
+            for i in range(1, n_new_rois+1):
+                self.model.shapes_layer.properties["track_axis"][-i] = scroll_axis
+                self.model.shapes_layer.properties["end_um"][-i] = self.model.max_px[scroll_axis]
+                self.model.shapes_layer.properties["start_um"][-i] = self.model.min_px[scroll_axis]
 
         # Ensure we have enough labels
         while len(self.gui.rois_gui) < n:
             self.gui.rois_gui.append(Label())
 
         while len(self.gui.rois_gui) > n:
-            self.gui.rois_gui.pop()
-
+            self.gui.rois_gui.pop()        
+            
         # Keep properties consistent
         self.model.sync_properties()
 
         # Update display text
+        idx_to_axis = {0: "Z", 1: "Y", 2: "X"}
         for i in range(n):
+
+            curr_axis = self.model.get_track_axis(i)
+            axis = idx_to_axis[curr_axis]
             self.gui.rois_gui[i].value = (
                 f"<b>ROI {i:02}</b>: "
-                f"Z start={self.model.get_z_start_px(i)}, "
-                f"Z end={self.model.get_z_end_px(i)}"
+                f"{axis} start={self.model.get_scroll_start_px(i)}, "
+                f"{axis} end={self.model.get_scroll_end_px(i)}"
             )
 
     def on_set_start(self):
@@ -58,9 +82,10 @@ class CroppingController:
         if idx is None:
             show_warning("Select exactly one cropping box.")
             return
-
-        z = self.model.viewer.dims.current_step[-3]
-        self.model.set_z_start_um(idx, z)
+        
+        curr_axis = self.model.get_track_axis(idx)
+        slice_idx = self.model.viewer.dims.current_step[curr_axis]
+        self.model.set_scroll_start_um(idx, slice_idx)
         self.update_rois()
 
     def on_set_stop(self):
@@ -68,9 +93,10 @@ class CroppingController:
         if idx is None:
             show_warning("Select exactly one cropping box.")
             return
-
-        z = self.model.viewer.dims.current_step[-3]
-        self.model.set_z_end_um(idx, z)
+        
+        curr_axis = self.model.get_track_axis(idx)
+        slice_idx = self.model.viewer.dims.current_step[curr_axis]
+        self.model.set_scroll_end_um(idx, slice_idx)
         self.update_rois()
 
     def on_clear_rois(self):
@@ -88,5 +114,4 @@ class CroppingController:
             return
 
         saved = self.model.save_csv(out_path, self.gui.tag_edit.value)
-        #self.gui.set_message(f"Saved ROI coordinates to {saved.name}")
         show_info(f"ROI coordinates saved to {saved.name}!")
--- src/napari_crop_tool/cropping/model.py
+++ src/napari_crop_tool/cropping/model.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 from pathlib import Path
 import numpy as np
 import pandas as pd
-
 from napari import Viewer
 from napari.layers import Shapes
 
@@ -23,13 +22,16 @@ class CroppingModel:
             "size": 12,
             "color": "white",
             "anchor": "upper_left",
-            "translation": [0, 0],
+            "translation": [0, 0, 0],
         }
 
-        # Compute default z range (in z-index units)
-        min_z_um, max_z_um, _ = self.viewer.dims.range[-3]
-        self.min_z = int(round(min_z_um / self.scale[0]))
-        self.max_z = int(round(max_z_um / self.scale[0]))
+        # Compute default range (in px index units)
+        min_um = np.array([self.viewer.dims.range[i][0] 
+                           for i in range(self.shapes_layer.ndim)])
+        max_um = np.array([self.viewer.dims.range[i][1] 
+                           for i in range(self.shapes_layer.ndim)])
+        self.min_px = np.round(min_um / np.array(self.scale)).astype(int)
+        self.max_px = np.round(max_um / np.array(self.scale)).astype(int)
 
     # ---- ROI helpers ----
     def num_rois(self) -> int:
@@ -41,26 +43,37 @@ class CroppingModel:
             return None
         return next(iter(sel))
 
-    def get_z_start_px(self, idx: int) -> int:
-        val = self.shapes_layer.properties["z_start_um"][idx]
-        return self.min_z if np.isnan(val) else int(val / self.scale[0])
+    def get_track_axis(self, idx: int) -> int:
+        val = self.shapes_layer.properties["track_axis"][idx]
+        return -1 if np.isnan(val) else int(val)
+    
+    def get_scroll_start_px(self, idx: int) -> int | float:
+        curr_axis = self.get_track_axis(idx)
+        val = self.shapes_layer.properties["start_um"][idx]
+        return (self.min_px[curr_axis] if np.isnan(val) 
+                else int(val / self.scale[curr_axis]))
 
-    def get_z_end_px(self, idx: int) -> int:
-        val = self.shapes_layer.properties["z_end_um"][idx]
-        return self.max_z if np.isnan(val) else int(val / self.scale[0])
+    def get_scroll_end_px(self, idx: int) -> int | float:
+        curr_axis = self.get_track_axis(idx)
+        val = self.shapes_layer.properties["end_um"][idx]
+        return (self.max_px[curr_axis] if np.isnan(val) 
+                else int(val / self.scale[curr_axis]))
 
-    def set_z_start_um(self, idx: int, z_index: int):
-        self.shapes_layer.properties["z_start_um"][idx] = z_index * self.scale[0]
+    def set_scroll_start_um(self, idx: int, curr_index: int):
+        curr_axis = self.get_track_axis(idx)
+        self.shapes_layer.properties["start_um"][idx] = curr_index * self.scale[curr_axis]
 
-    def set_z_end_um(self, idx: int, z_index: int):
-        self.shapes_layer.properties["z_end_um"][idx] = z_index * self.scale[0]
+    def set_scroll_end_um(self, idx: int, curr_index: int):
+        curr_axis = self.get_track_axis(idx)
+        self.shapes_layer.properties["end_um"][idx] = curr_index * self.scale[curr_axis]
 
     def clear_rois(self):
         self.shapes_layer.data = []
         self.shapes_layer.properties = {
-            "id": [],
-            "z_start_um": [],
-            "z_end_um": [],
+            "id": np.array([], dtype=str),
+            "start_um": np.array([], dtype=float),
+            "end_um": np.array([], dtype=float),
+            "track_axis" : np.array([], dtype=int)
         }
 
     def sync_properties(self):
@@ -68,29 +81,38 @@ class CroppingModel:
         n = self.num_rois()
         self.shapes_layer.properties = {
             "id": np.array([str(i) for i in range(n)], dtype=str),
-            "z_start_um": (np.array([self.get_z_start_px(i) for i in range(n)], 
+            "start_um": (np.array([self.get_scroll_start_px(i) for i in range(n)], 
                                     dtype=float) * self.scale[0]),
-            "z_end_um": (np.array([self.get_z_end_px(i) for i in range(n)], 
+            "end_um": (np.array([self.get_scroll_end_px(i) for i in range(n)], 
                                   dtype=float) * self.scale[0]),
+            "track_axis": (np.array([self.get_track_axis(i) for i in range(n)], 
+                                  dtype=int)),
         }
 
     # ---- saving ----
     def save_csv(self, out_path: Path, tag: str) -> Path:
         roi_df = pd.DataFrame(
-            columns=["x_start_um", "y_start_um", "x_end_um", "y_end_um", "z_start_um", "z_end_um"]
+            columns=["x_start_um", "y_start_um", "x_end_um", "y_end_um", 
+                     "z_start_um", "z_end_um"]
         )
 
+        id_to_axis = {0: "z", 1: "y", 2: "x"}
         for i, roi in enumerate(self.shapes_layer.data):
-            z0_um = self.get_z_start_px(i) * self.scale[0]
-            z1_um = self.get_z_end_px(i) * self.scale[0]
-            roi_df.loc[i] = {
-                "x_start_um": float(roi[:, 1].min()),
-                "y_start_um": float(roi[:, 0].min()),
-                "x_end_um": float(roi[:, 1].max()),
-                "y_end_um": float(roi[:, 0].max()),
-                "z_start_um": min(z0_um, z1_um),
-                "z_end_um": max(z0_um, z1_um),
-            }
+            roi_dict = {}
+            scroll_axis = self.shapes_layer.properties["track_axis"][i]
+            for axis in (0, 1, 2):
+                if axis == scroll_axis:
+                    start_um = self.get_scroll_start_px(i) * self.scale[scroll_axis]
+                    end_um = self.get_scroll_end_px(i) * self.scale[scroll_axis]
+                    start_um = min(start_um, end_um)
+                    end_um = max(start_um, end_um)
+                else:
+                    start_um = roi[:, axis].min()
+                    end_um = roi[:, axis].max()
+                
+                roi_dict[f"{id_to_axis[axis]}_start_um"] = np.round(start_um, 3)
+                roi_dict[f"{id_to_axis[axis]}_end_um"] = np.round(end_um, 3)
+            roi_df.loc[i] = roi_dict
 
         prefix = f"{tag}_roi_" if tag else "roi_"
         roi_df.index = [f"{prefix}{i:02}" for i in range(len(roi_df))]
