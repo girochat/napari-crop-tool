f8bbfe7ab32e10b76a024e295e2fbc3cc8a7ae9d
girochat <117285316+girochat@users.noreply.github.com>
2026-08-01 19:14:01 +0200

    fix: set napari setting shape highlight to transparent to avoid highly anisotropic display

--- src/napari_crop_tool/widget.py
+++ src/napari_crop_tool/widget.py
@@ -3,6 +3,7 @@ from qtpy.QtWidgets import (
 )
 
 from napari import Viewer
+from napari.settings import get_settings
 from .layer_selection.controller import LayerSelectionControllerQt
 
 
@@ -12,7 +13,22 @@ class MainWidgetQt(QWidget):
         self.viewer = viewer
         self.entry_controller = LayerSelectionControllerQt(viewer)
 
+        # Save current napari highlight setting
+        highlight_settings = get_settings().appearance.highlight
+        self._previous_highlight_color = (
+            highlight_settings.highlight_color
+        )
+
+        # Use transparent value for highlight color (avoid big anisotropic displays)
+        highlight_settings.highlight_color = [0, 0, 0, 0]
+
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 10)
         layout.addWidget(self.entry_controller.layer_gui)
         layout.addWidget(self.entry_controller.cropping_gui, stretch=1)
+
+    def closeEvent(self, event):
+        """Restore napari highlight settings on close."""
+        highlight_settings = get_settings().appearance.highlight
+        highlight_settings.highlight_color = self._previous_highlight_color
+        super().closeEvent(event)
